18942833d432bf7c38c5d08cea66a4c8b669a7ec
gsmith98 <smitgw11@gmail.com>
2017-04-20 19:39:02 -0700

    step 2

--- reactApp/app.js
+++ reactApp/app.js
@@ -2,7 +2,28 @@ import 'babel-polyfill';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+
+class TimeSlot extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { busy: false }
+  }
+
+  render() {
+    return (
+      <div
+        onClick={() => this.setState({ busy: !this.state.busy })}
+        style={{ backgroundColor: (this.state.busy ? 'red' : 'green')}}
+      >
+        YO
+      </div>
+    );
+  }
+}
+
+
 render(
-  <p>YO</p>,
+  <TimeSlot />,
   document.getElementById('root')
 );
