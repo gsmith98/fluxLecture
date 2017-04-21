5458070b89642b183f3bdd742f23b3499b31a670
gsmith98 <smitgw11@gmail.com>
2017-04-20 23:55:24 -0700

    step 3

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,6 +1,21 @@
 import 'babel-polyfill';
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { Dispatcher } from 'flux';
+import { EventEmitter } from 'events';
+
+
+var AppDispatcher = new Dispatcher();
+
+var busyStore =  Object.assign({}, EventEmitter.prototype, { busy: false });
+
+AppDispatcher.register( function( payload ) {
+    switch( payload.actionName ) {
+        case 'toggle':
+            busyStore.busy = !busyStore.busy;
+            busyStore.emit('change')
+    }
+});
 
 
 class TimeSlot extends Component {
@@ -8,12 +23,31 @@ class TimeSlot extends Component {
     super(props);
 
     this.state = { busy: false }
+    this.localUpdate = this.localUpdate.bind(this);
+  }
+
+  localUpdate() {
+    this.setState({ busy: busyStore.busy })
+  }
+
+  componentDidMount() {
+    busyStore.on('change', this.localUpdate);
+  }
+
+  componentWillUnmount() {
+    buysStore.removeListener('change', this.localUpdate);
+  }
+
+  clickSlot() {
+    AppDispatcher.dispatch({
+        actionName: 'toggle'
+    });
   }
 
   render() {
     return (
       <div
-        onClick={() => this.setState({ busy: !this.state.busy })}
+        onClick={this.clickSlot}
         style={{ backgroundColor: (this.state.busy ? 'red' : 'green')}}
       >
         YO
